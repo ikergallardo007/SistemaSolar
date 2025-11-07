fa32b9be029b1d5843492ee33b851c5941bb5dfd
Iker Gallardo Arrieta <ikergallardo22@gmail.com>
2025-11-07 17:45:55 +0100

    Main Script
    Main script created. Sun, Earth and Moon transforms created.

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// Main class that controls all the Solar System.
+public class Main : MonoBehaviour
+{
+    // Public Properties
+    public Transform sunTransform;
+    public Transform earthTransform;
+    public Transform moonTransform;
+
+
+    // Private Attributes
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
