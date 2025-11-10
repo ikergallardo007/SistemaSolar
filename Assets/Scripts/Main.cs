399acc1ff4f29cf889e26a91cf718bf163344bf3
Iker Gallardo Arrieta <ikergallardo22@gmail.com>
2025-11-10 12:05:45 +0100

    Final Commit
    Debug,DrawRay functions implemented.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,8 +11,9 @@ public class Main : MonoBehaviour
     public Transform earthTransform;
     public Transform moonTransform;
 
-
     // Private Attributes
+    private Vector3 earthDirectionVector;
+    private Vector3 moonDirectionVector;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,8 +24,29 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Rotation of sun, earth and moon.
         sunTransform.Rotate(Vector3.up, sunRotationSpeed *  Time.deltaTime);
         earthTransform.Rotate(Vector3.up, earthRotationSpeed * Time.deltaTime);
         moonTransform.Rotate(Vector3.up, moonRotationSpeed * Time.deltaTime);
+
+        // Direction vectors.
+        //earthDirectionVector = earthTransform.forward * (-1);
+
+        // Rays of the sun.
+        Debug.DrawRay(sunTransform.position, sunTransform.right, Color.red); 
+        Debug.DrawRay(sunTransform.position, sunTransform.up, Color.green);
+        Debug.DrawRay(sunTransform.position, sunTransform.forward, Color.blue);
+
+        // Rays of the earth.
+        Debug.DrawRay(earthTransform.position, earthTransform.right, Color.red);
+        Debug.DrawRay(earthTransform.position, earthTransform.up, Color.green);
+        Debug.DrawRay(earthTransform.position, earthTransform.forward, Color.blue);
+        Debug.DrawRay(earthTransform.position, sunTransform.forward * (-1));
+
+        // Rays of the moon.
+        Debug.DrawRay(moonTransform.position, moonTransform.right, Color.red);
+        Debug.DrawRay(moonTransform.position, moonTransform.up, Color.green);
+        Debug.DrawRay(moonTransform.position, moonTransform.forward, Color.blue);
+        Debug.DrawRay(moonTransform.position, earthTransform.forward * (-1));
     }
 }
